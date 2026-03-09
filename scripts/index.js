d32291eed69bb07d942adec2275b8a86a2994fb7
arrimon <rsnrimon17@gmail.com>
2026-03-09 23:19:41 +0600

    Search Button working Properly

--- scripts/index.js
+++ scripts/index.js
@@ -201,3 +201,19 @@ const displayLesson = (lesson) => {
 
 loadLessonData()
 
+document.getElementById('btn-search').addEventListener('click', ()=>{
+    const input = document.getElementById('input-search');
+    const searchValue = input.value.trim().toLowerCase();
+    console.log(searchValue)
+
+    fetch('https://openapi.programming-hero.com/api/words/all')
+        .then(res => res.json())
+        .then(data => {
+            const allWords = data.data;
+            // console.log(allWords);
+            const filterWords = allWords.filter(word => word.word.toLowerCase().includes(searchValue));
+            // console.log(filterWords)
+            displayLevelWord(filterWords)
+        })
+
+})
